c8c04d21ef070975a993f6f675c73fdef3433122
superjkli <superjkli@gmail.com>
2019-09-07 17:39:04 +0900

    q1

---
+++ ch2/q1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using std::cout,std::endl, std::string;
+
+int main (){
+    string name = "John", address = "New Taipei City, Taiwan";
+    cout << "My name: " << name << endl;
+    cout << "My address: " << address << endl;
+    return 0;
+}
