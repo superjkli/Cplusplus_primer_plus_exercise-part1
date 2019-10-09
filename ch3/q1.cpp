83f7d68b6c32ea6efb706622dd5e5e4d6b6e34c2
superjkli <superjkli@gmail.com>
2019-10-09 16:52:33 +0900

    ch3 q1 finished

--- ch3/q1.cpp
+++ ch3/q1.cpp
@@ -1,1 +1,14 @@
 #include<iostream>
+
+using std::cout, std::endl;
+
+int main() {
+    int inches, inch, feet = 0;
+    const int inch_to_feet = 12;
+    cout << "Enter your height in inches: _____\b\b\b\b\b";
+    std::cin >> inches;
+    inch = inches % inch_to_feet;
+    feet = (inches - inch) / inch_to_feet;
+    cout << "Your height is " << feet << " feet " << inch << " inches." << endl;
+    return 0;
+}
