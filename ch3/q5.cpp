90e7c5cb10de8a849bd5a0d296cd78f4d485713d
superjkli <superjkli@gmail.com>
2019-10-23 14:03:21 +0900

    q5 halfway

--- ch3/q5.cpp
+++ ch3/q5.cpp
@@ -1,2 +1,21 @@
 #include<iostream>
 
+using std::cout, std::cin, std::endl;
+
+int main() {
+    //variazbles
+    long long world_population = 0, US_population = 0;
+    double percentage;
+
+    //data get
+    cout << "Enter the world's population: ";
+    cin >> world_population;
+    cout << "Enter the population of the US: ";
+    cin >> US_population;
+    
+    //calculation
+    percentage = (long double) US_population * 100.0 / world_population;
+
+    cout << "The population of the US is " << percentage << " %"
+
+
