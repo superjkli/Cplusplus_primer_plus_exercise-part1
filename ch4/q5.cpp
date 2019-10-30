0122bd80390d3ee1d3e28c8b1db7e3540ddcf411
superjkli <superjkli@gmail.com>
2019-10-30 14:24:17 +0900

    q5 done

--- ch4/q5.cpp
+++ ch4/q5.cpp
@@ -1,1 +1,23 @@
 #include<iostream>
+#include<string>
+
+using std::cout, std::cin, std::string, std::endl, std::getline;
+
+struct CandyBar {
+    string brand_name;
+    double weight;
+    int calories; 
+};
+
+
+int main() {
+    CandyBar snack = {
+        "Mocha Munch",
+        2.3,
+        350
+    };
+    cout << "Brand name: " << snack.brand_name << endl;
+    cout << "Weight: " << snack.weight << endl;
+    cout << "Calories: " << snack.calories << endl;
+    return 0;
+}
