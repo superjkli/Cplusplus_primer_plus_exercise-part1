ebcfdaf705c1164d74493c8f164c21f70f8f1cb8
superjkli <superjkli@gmail.com>
2019-10-30 14:11:52 +0900

    q4 done

--- ch4/q4.cpp
+++ ch4/q4.cpp
@@ -1,1 +1,18 @@
 #include<iostream>
+#include<string>
+
+using std::cout, std::cin, std::endl, std::string, std::getline;
+
+int main() {
+    string firstname, lastname, result;
+
+    cout << "Enter your first name: ";
+    getline(cin, firstname);
+    cout << "Enter your last name: ";
+    getline(cin, lastname);
+
+    result = lastname + ", " + firstname;
+
+    cout << "Here's the infomation in a single string: " << result << endl;
+    return 0;
+}
