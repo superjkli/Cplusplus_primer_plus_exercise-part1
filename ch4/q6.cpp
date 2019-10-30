287a79ac21a61441b3de0490a224a2ff76bd1a69
superjkli <superjkli@gmail.com>
2019-10-30 14:45:32 +0900

    q7 finished

--- ch4/q6.cpp
+++ ch4/q6.cpp
@@ -1,1 +1,34 @@
 #include<iostream>
+#include<string>
+
+using std::cout, std::cin, std::string, std::endl, std::getline;
+
+struct CandyBar {
+    string brand_name;
+    double weight;
+    int calories; 
+};
+
+
+int main() {
+    CandyBar a = { "Choco Bar", 20.9, 500};
+    CandyBar b = { "Vanila ice", 500.012, 2139};
+    CandyBar c = { "KMR", 114.514, 810};
+
+    cout << "Brand name: " << a.brand_name << endl;
+    cout << "Weight: " << a.weight << endl;
+    cout << "Calories: " << a.calories << endl;
+    cout << endl;
+
+    cout << "Brand name: " << b.brand_name << endl;
+    cout << "Weight: " << b.weight << endl;
+    cout << "Calories: " << b.calories << endl;
+    cout << endl;
+
+    cout << "Brand name: " << c.brand_name << endl;
+    cout << "Weight: " << c.weight << endl;
+    cout << "Calories: " << c.calories << endl;
+    cout << endl;
+
+    return 0;
+}
--- ch4/q7.cpp
+++ ch4/q7.cpp
@@ -1,1 +1,30 @@
 #include<iostream>
+#include<string>
+
+using std::cin, std::cout, std::endl, std::string, std::getline;
+
+struct Pizza {
+    string company_name;
+    double diameter;
+    double weight;
+};
+
+int main() {
+    Pizza your_choice;
+    
+    cout << "Enter pizza company name: ";
+    getline(cin, your_choice.company_name);
+    cout << "Enter pizza diameter: ";
+    cin >> your_choice.diameter;
+    cout << "Enter weight: ";
+    cin >> your_choice.weight;
+
+    cout << endl
+
+    cout << "Pizza company name: " << your_choice.company_name << endl;
+    cout << "Pizza diameter: " << your_choice.diameter << endl;
+    cout << "Pizza weight: " << your_choice.weight << endl;
+
+    return 0;
+}
+
