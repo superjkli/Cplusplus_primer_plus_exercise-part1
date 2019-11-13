2b3b9a1250efe71fc8c5743d2dd3cf4f4b289fd6
superjkli <superjkli@gmail.com>
2019-11-13 15:46:43 +0900

    q9 q10 done

--- ch4/q10.cpp
+++ ch4/q10.cpp
@@ -1,1 +1,23 @@
 #include<iostream>
+#include<array>
+#include<cstdio>
+#include<iomanip>
+using std::cout, std::cin, std::endl, std::array, std::printf;
+
+int main () {
+    array<double, 3> dash;
+    
+    //enter data
+    cout << "Enter your times for a 40 meter dash" << endl;
+    cout << "time #1: ";
+    cin >> dash[0];
+    cout << "time #2: ";
+    cin >> dash[1];
+    cout << "time #3: ";
+    cin >> dash[2];
+    //output
+    printf("your times: %.2f, %.2f, %.2f \n", dash[0], dash[1], dash[2]);
+    cout << "average: " << std::fixed << std::setprecision(2)  << (dash[0] + dash[1] + dash[2]) / 3.0 << endl;
+    return 0;
+}
+
--- ch4/q9.cpp
+++ ch4/q9.cpp
@@ -1,1 +1,35 @@
 #include<iostream>
+#include<string>
+
+using std::cout, std::cin, std::string, std::endl, std::getline;
+
+struct CandyBar {
+    string brand_name;
+    double weight;
+    int calories; 
+};
+
+
+int main() {
+    auto candies = new CandyBar[3];
+    candies[0] = { "Choco Bar", 20.9, 500};
+    candies[1] = { "Vanila ice", 500.012, 2139};
+    candies[2] = { "KMR", 114.514, 810};
+
+    cout << "Brand name: " << candies[0].brand_name << endl;
+    cout << "Weight: " <<candies[0].weight << endl;
+    cout << "Calories: " << candies[0].calories << endl;
+    cout << endl;
+
+    cout << "Brand name: " << candies[1].brand_name << endl;
+    cout << "Weight: " << candies[1].weight << endl;
+    cout << "Calories: " << candies[1].calories << endl;
+    cout << endl;
+
+    cout << "Brand name: " << candies[2].brand_name << endl;
+    cout << "Weight: " << candies[2].weight << endl;
+    cout << "Calories: " << candies[2].calories << endl;
+    cout << endl;
+
+    return 0;
+}
